e4bd81c5860b261fef8bda35841822bb99541742
lisna <lisnawatispentira@gmail.com>
2024-08-15 11:19:27 +0700

    menambahkan file helloword.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+
+$nama = "Lisnawati";
+$umur = "17";
+$ttl = "Sumedang,12 juli 200*";
+$askot = "Sumedang";
+$asalsekolah = "Smk negeri 2 sumedang";
+
+echo " ";
+echo "Name : $nama". "<br>";
+echo "Umur : $umur". "<br>";
+echo "Ttl : $ttl". "<br>";
+echo "Askot : $askot". "<br>";
+echo "Asalsekolah : $asalsekolah". "<br>";
+
+
+
+// *(tipe data boolean)
+// echo "Benar";
+// echo true;
+
+// echo "False";
+// echo false;
+
+
+
+// *(tipe data Double quete)
+//$nama = " lisnawati";
+//echo 'nama : ';
+//echo 'Lisnawati $nama Eliss';
+//echo "\n";
+
+?>
